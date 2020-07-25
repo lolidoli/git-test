d1301ec68ceb54965d982f45c349a68ce07be36f
Yuri <yuri.timochkin@me.com>
2020-07-25 10:55:01 +0700

    добавил условия

---
+++ uslovia.js
@@ -0,0 +1,33 @@
+"use strict";
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log("error");
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много')
+} else {
+    console.log('Ok!');
+}
+
+(num == 50) ? console.log('ok') : console.log('не принято');
+
+const numo = 50;
+
+switch (numo) {
+    case 49:
+        console.log('wrong)');
+        break;
+    case 51:
+        console.log('also worng');
+        break;
+    default:
+        console.log('the right answer was 50');
+        break;
+}
