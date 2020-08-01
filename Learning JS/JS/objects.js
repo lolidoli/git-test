60200c681ce402c648a2435b55422e8fc3f84dff
Yuri <yuri.timochkin@me.com>
2020-08-01 18:00:10 +0700

    objects

--- Learning JS/JS/objects.js
+++ Learning JS/JS/objects.js
@@ -10,4 +10,24 @@ const options = {
     }
 };
 
-console.log(options.name);
+console.log(options['color']['border']);
+console.log(options.color.border);
+
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+//Для перебора можно использовать метод for in и для вложенного объекта вот такую конструкцию
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+     for (let i in options[key]) {
+         console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+     }
+    } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+
+
+
